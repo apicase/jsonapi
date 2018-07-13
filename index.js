4f7a7169853512a95b7dce3594b52684c0d55a84
Anton Kosykh <kelin2025@yandex.ru>
2018-07-13 15:52:15 +0300

    Fixed `next` call

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ export const jsonApiPlugin = function jsonApiPlugin(service) {
   return service.extend({
     hooks: {
       done: function jsonApiDoneHook(ctx) {
-        if (!ctx.meta.normalize) return next(ctx.result)
+        if (!ctx.meta.normalize) return ctx.next(ctx.result)
         ctx.result.rawBody = ctx.result.body
         ctx.result.body = normalize(ctx.result.body, ctx.meta.normalize)
-        next(ctx.result)
+        ctx.next(ctx.result)
       }
     }
   })
